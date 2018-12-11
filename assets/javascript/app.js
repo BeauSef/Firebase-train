f25c0bd67469d12bb6253b6923a1abab6f3a8cdc
Beau Hunter <hunter0907@gmail.com>
2018-12-11 12:38:39 -0700

    Added Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCFKBiuWbMJc5mT_mr9p_Tjk27zgSedbmQ",
+    authDomain: "trainschedule-bd630.firebaseapp.com",
+    databaseURL: "https://trainschedule-bd630.firebaseio.com",
+    projectId: "trainschedule-bd630",
+    storageBucket: "trainschedule-bd630.appspot.com",
+    messagingSenderId: "215121461101"
+  };
+  firebase.initializeApp(config);
